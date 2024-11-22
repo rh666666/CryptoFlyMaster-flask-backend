2e700a18ac37f24d2c20bbbfd0b77fb23c33aa00
chen22222222 <2094456563@qq.com>
2024-11-22 16:50:03 +0800

    添加了AES，SM4加解密模块

--- test.py
+++ test.py
@@ -4,15 +4,58 @@ from flask_cors import CORS
 #DES加密
 from hao import DES
 #AES加密
-from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
-from cryptography.hazmat.backends import default_backend
-from cryptography.hazmat.primitives import padding
-import base64
+from AES import AES
+#SM4加密
+from SM4 import SM4
+
 
 app = Flask(__name__)
 CORS(app)  # 允许跨域访问
 
-# AES 加解密逻辑
+#AES加解密逻辑
+def main():
+        choice = int(input("请输入 1 进行加密, 2 进行解密: "))
+        if choice not in [1, 2]:
+            print("无效选择")
+            return
+        
+        Text = input("请输入文本:")
+        if choice == 1:
+            Text_hex = ''.join(format(ord(char), '02x') for char in Text if char.isprintable())
+            data_bytes = bytes.fromhex(Text_hex)
+            Text_hex = pkcs7_pad(data_bytes).hex()
+        else:
+            Text_hex = Text
+
+        Key = input("请输入密钥(128位,16字节):") 
+        Key_hex = ''.join(format(ord(char), '02x') for char in Key if char.isprintable())
+
+        if len(Key) != 16:
+            print("密钥长度不为 128 位。")
+            return
+        
+        result = ''
+  
+        for i in range(0, len(Text_hex),32):
+            temp_str1 = Text_hex[i:i+32]
+            result_list = AES.Encryption(temp_str1, Key_hex) if choice == 1 else AES.Decryption(temp_str1, Key_hex)
+            for i in range(4):
+                for j in range(4):
+                    result += result_list[j][i].lstrip("0x").zfill(2)
+  
+        if result:
+            if choice == 1:
+                print("加密后的密文:"+ result)
+            else:
+                data_bytes = bytes.fromhex(result)
+                result = pkcs7_unpad(data_bytes).hex()
+                byte_data = bytes.fromhex(result)
+                result = ''.join(chr(b) for b in byte_data)
+                print("解密后的明文:"+ result)
+        else:
+            print("处理失败，请检查输入。")
+
+'''# AES 加解密逻辑
 def aes_encrypt(data, key):
     key = key.encode("utf-8").ljust(16)[:16]  # 确保密钥长度为 16 字节
     cipher = Cipher(algorithms.AES(key), modes.ECB(), backend=default_backend())
@@ -38,8 +81,9 @@ def aes_decrypt(data, key):
     unpadder = padding.PKCS7(128).unpadder()
     unpadded_data = unpadder.update(decrypted) + unpadder.finalize()
     return unpadded_data.decode("utf-8")
+'''
 
-#DES加密逻辑
+#DES加解密逻辑
 def pkcs7_pad(data: bytes, block_size: int = 8) -> bytes:
     """
     对数据进行 PKCS#7 填充。
@@ -62,11 +106,11 @@ def pkcs7_unpad(data: bytes) -> bytes:
         raise ValueError("Invalid padding.")
     return data[:-padding_len]
 
-def shuruzhuanhuan_encrypt(Text_hex):
+def shuruzhuanhuan_encrypt(Text_hex,i):
     '''加密时对输入的文本进行格式转换，并进行填充'''
     Text_hex = ''.join(format(ord(char), '02x') for char in Text_hex if char.isprintable())
     data_bytes = bytes.fromhex(Text_hex)
-    Text_hex = pkcs7_pad(data_bytes).hex()
+    Text_hex = pkcs7_pad(data_bytes,i).hex()
     return Text_hex
 
 def secret_padding(key_hex):
@@ -86,15 +130,33 @@ def encrypt():
 
         # 根据算法调用加密函数（这里只实现了 AES，DES）
         if algorithm == 'AES':
-            result = aes_encrypt(data, key)
+            data = shuruzhuanhuan_encrypt(data,16)  # 进行格式转换和填充
+            key = secret_padding(key)    # 进行密钥填充
+            result = ''
+            for i in range(0, len(data),32):
+                temp_str1 = data[i:i+32]
+                result_list = AES.Encryption(temp_str1, key) 
+                for i in range(4):
+                    for j in range(4):
+                        result += result_list[j][i].lstrip("0x").zfill(2)
+
         elif algorithm == 'DES':
-            data = shuruzhuanhuan_encrypt(data)  # 进行格式转换和填充
+            data = shuruzhuanhuan_encrypt(data,8)  # 进行格式转换和填充
             key = secret_padding(key)    # 进行密钥填充
 
             result = ''
             for i in range(0, len(data),16):
                 temp_str1 = data[i:i+16]
                 result = result + DES.encryption(temp_str1, key) 
+        
+        elif algorithm == 'SM4':
+            data = shuruzhuanhuan_encrypt(data,16)  # 进行格式转换和填充
+            key = secret_padding(key)    # 进行密钥填充
+
+            result = ''
+            for i in range(0, len(data),32):
+                temp_str1 = data[i:i+32]
+                result = result + SM4.encryption(temp_str1, key) 
         else:
             return jsonify({'error': 'Unsupported algorithm'}), 400
 
@@ -102,9 +164,17 @@ def encrypt():
         return jsonify({'result': result})
     except Exception as e:
         # 如果发生错误，返回错误信息
-        result = '请输入八字节的密钥'  # 自定义的错误内容
-        return jsonify({'result': result, 'error': str(e)}), 500
-#        return jsonify({'error': str(e)}), 500
+        if algorithm == 'AES':
+            result = '请输入16字节的密钥,采用utf-8编码'  # 自定义的错误内容
+            return jsonify({'result': result, 'error': str(e)}), 500
+        elif algorithm == 'DES':
+            result = '请输入8字节的密钥,采用utf-8编码'  # 自定义的错误内容
+            return jsonify({'result': result, 'error': str(e)}), 500
+        elif algorithm == 'SM4':
+            result = '请输入16字节的密钥,采用utf-8编码'  # 自定义的错误内容
+            return jsonify({'result': result, 'error': str(e)}), 500
+        else: 
+            return jsonify({'error': str(e)}), 500
 
 @app.route('/decrypt', methods=['POST'])
 def decrypt():
@@ -114,7 +184,20 @@ def decrypt():
         algorithm = request.json.get('algorithm', 'AES')
 
         if algorithm == 'AES':
-            result = aes_decrypt(data, key)
+            key = secret_padding(key)    # 进行密钥编码
+            
+            result = ''
+            for i in range(0, len(data),32):
+                temp_str1 = data[i:i+32]
+                result_list = AES.Decryption(temp_str1, key)
+                for i in range(4):
+                    for j in range(4):
+                        result += result_list[j][i].lstrip("0x").zfill(2)
+            data_bytes = bytes.fromhex(result)
+            result = pkcs7_unpad(data_bytes).hex()
+            byte_data = bytes.fromhex(result)
+            result = ''.join(chr(b) for b in byte_data)
+
         elif algorithm == 'DES':
             key = secret_padding(key)    # 进行密钥填充
             
@@ -127,13 +210,30 @@ def decrypt():
             byte_data = bytes.fromhex(result)
             result = ''.join(chr(b) for b in byte_data)
 
+        elif algorithm == 'SM4':
+            key = secret_padding(key)    # 进行密钥填充
+            
+            result = ''
+            for i in range(0, len(data),32):
+                temp_str1 = data[i:i+32]
+                result = result + SM4.decryption(temp_str1, key)
+            data_bytes = bytes.fromhex(result)
+            result = pkcs7_unpad(data_bytes).hex()
+            byte_data = bytes.fromhex(result)
+            result = ''.join(chr(b) for b in byte_data)
         else:
             return jsonify({'error': 'Unsupported algorithm'}), 400
 
         return jsonify({'result': result})
     except Exception as e:
-        result = '检查输入的密钥(八字节)和密文(正确格式)是否正确'  # 自定义的错误内容
-        return jsonify({'result': result, 'error': str(e)}), 500
+        if algorithm == 'AES':
+            result = '请输入16字节的密钥,采用utf-8编码'  # 自定义的错误内容
+            return jsonify({'result': result, 'error': str(e)}), 500
+        elif algorithm == 'DES':
+            result = '请输入8字节的密钥,采用utf-8编码'  # 自定义的错误内容
+            return jsonify({'result': result, 'error': str(e)}), 500
+        else:
+            return jsonify({'error': str(e)}), 500
 #        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
